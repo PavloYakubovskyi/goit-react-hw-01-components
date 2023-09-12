b0094f0d14725fafab4cc77ed661ac175b0dba9f
Pavlo Yakubovskyi <pavloyakubovskyi@gmail.com>
2023-09-12 23:09:00 +0200

    added new project vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import user from "./assets/user.json";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+}
+
+export default App;
